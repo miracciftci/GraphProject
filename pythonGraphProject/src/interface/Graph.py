56ec833fc1f3188f1a046da2378f251ccaa2b92d
dogukan <dogukandogru06@gmail.com>
2023-05-24 23:11:47 +0300

    added additional slicing to node label

--- pythonGraphProject/src/interface/Graph.py
+++ pythonGraphProject/src/interface/Graph.py
@@ -35,16 +35,23 @@ if __name__ == "__main__":
         pos = nx.circular_layout(G)
         colors = nx.get_edge_attributes(G,'color').values()
         edge_values = {(f,s):d["edge_value"] for f,s,d in G.edges(data=True)}
-        for t,f,s in G.nodes(data=True):
-           print(f"s printing {s}")
+        # for f,s in G.nodes(data=True):
+        #    if len(s) != 0:
+        #         print(f"s printing {s['size']}")
         node_values = {f:s for f,s in G.nodes(data=True)}
-        print(node_values)
+        new_node_values={}
+        for i in node_values:
+            if len(node_values[i]) != 0:
+                #print(f" i  ==  {node_values[i]['size']}")
+                new_node_values[i] = node_values[i]['size']
+        print(new_node_values)         
+        #print(node_values)
 
         #G.add_edge(6,1)
     labels = nx.get_node_attributes(G,'size')
     #pos_higher = getPosHigher(pos)
     nx.draw(G,pos,edge_color = colors,node_color = 'lightgreen' ,with_labels=True)
-    nx.draw_networkx_labels(G,pos=pos , labels = node_values,font_color = "black",font_size = 12)
+    nx.draw_networkx_labels(G,pos=pos , labels = node_values,font_color = "blue",font_size = 12)
     nx.draw_networkx_edge_labels(G,pos = pos,edge_labels = edge_values, label_pos = 0.5)
     plt.margins(0.2)
     plt.show()
