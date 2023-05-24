568ba36e47f332d4a0c204fdbf9cc08882140046
dogukan <dogukandogru06@gmail.com>
2023-05-25 00:43:27 +0300

    added summary and model options to file input file

--- pythonGraphProject/src/interface/FileInput.py
+++ pythonGraphProject/src/interface/FileInput.py
@@ -13,7 +13,7 @@ class Ui_Form(object):
         Form.setObjectName("Form")
         Form.resize(500, 350)
         self.Buttonpush = QtWidgets.QPushButton(Form)
-        self.Buttonpush.setGeometry(QtCore.QRect(85, 134, 111, 34))
+        self.Buttonpush.setGeometry(QtCore.QRect(85, 130, 180, 34))
         self.Buttonpush.setStyleSheet("background-color:blue;\n"
                                       "font:bold 15px;\n"
                                       "padding :7px;\n"
@@ -28,7 +28,41 @@ class Ui_Form(object):
                                       "\n"
                                       "")
         self.Buttonpush.setObjectName("Buttonpush")
+        self.Buttonpush2 = QtWidgets.QPushButton(Form)
+        self.Buttonpush2.setGeometry(QtCore.QRect(85, 190, 180, 34))
+        self.Buttonpush2.setStyleSheet("background-color:blue;\n"
+                                      "font:bold 15px;\n"
+                                      "padding :7px;\n"
+                                      "min-width:12px;\n"
+                                      "color: white;\n"
+                                      "border-radius:12px;\n"
+                                      "border-color:blue;\n"                                    
+                                      "border-style: outset;\n"
+                                      "border-width:2px;\n"
+                                      
+                                      "\n"
+                                      "\n"
+                                      "")
+        self.Buttonpush2.setObjectName("Buttonpush2")
+        self.ButtonpushConnect = QtWidgets.QPushButton(Form)
+        self.ButtonpushConnect.setGeometry(QtCore.QRect(260, 250, 180, 34))
+        self.ButtonpushConnect.setStyleSheet("background-color:black;\n"
+                                      "font:bold 18px;\n"
+                                      "padding :7px;\n"
+                                      "min-width:12px;\n"
+                                      "color: white;\n"
+                                      "border-radius:12px;\n"
+                                      "border-color:blue;\n"                                    
+                                      "border-style: outset;\n"
+                                      "border-width:2px;\n"
+                                      
+                                      "\n"
+                                      "\n"
+                                      "")
+        self.ButtonpushConnect.setObjectName("ButtonpushConnect")
         self.retranslateUi(Form)
+        self.retranslateUi2(Form)
+        self.retranslateUiConnect(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
 
@@ -38,12 +72,42 @@ class Ui_Form(object):
         Form.setWindowTitle(_translate("Form", "Form"))
         self.Buttonpush.setText(_translate("Form", "Browse File"))
         self.Buttonpush.clicked.connect(self.Buttonpush_handler)
+    
+    def retranslateUi2(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.Buttonpush2.setText(_translate("Form", "Browse Summary File"))
+        self.Buttonpush2.clicked.connect(self.Buttonpush_handler)
+    
+    def retranslateUiConnect(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.ButtonpushConnect.setText(_translate("Form", "Model"))
+        self.ButtonpushConnect.clicked.connect(self.Buttonpush_handlerModel)
 
 
     def Buttonpush_handler(self):
         print("Button pressed")
         self.open_dialog_box()
-        
+    
+    def Buttonpush_handler2(self):
+        print("Button pressed")
+        self.open_dialog_box2()
+
+    def Buttonpush_handlerModel(self):
+        print("Button pressed")
+        self.open_dialog_box_model()
+    
+    def open_dialog_box2(self):
+        filename2 = QFileDialog.getOpenFileName()
+        filepath2 = filename2[0]
+
+        with open(filepath2, "r") as file:
+            Input.textOzet = file.readlines()
+    
+    def open_dialog_box_model(self):
+        print("modelling")
+    
     def open_dialog_box(self):
         filename = QFileDialog.getOpenFileName()
         filepath = filename[0]
